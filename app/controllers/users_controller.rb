dbdc67d61fb24085f870ccf9061ff2391881f15a
HALv2 <charlotte1787@gmail.com>
2020-07-06 18:43:38 +0900

    make group_edit asynchronous

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,27 @@
 class UsersController < ApplicationController
- def edit  
- end
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |form|
+      form.html
+      form.json
+    end
+  end
+  
+  def edit  
+  end
 
- def update
-   if current_user.update(user_params)
-     redirect_to root_path
-   else
-    render :edit
-   end 
- end
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+     end 
+  end
 
- private
+  private
  
- def user_params
-  params.require(:user).permit(:name, :email)
- end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
